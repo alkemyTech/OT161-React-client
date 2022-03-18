035cb92ddcedac145e6de05241dab30b7750d463
Jose Manuel Rosales <josemanuelrosales2@gmail.com>
2022-03-18 00:52:34 -0600

    Form validation & styles: validation working without request

--- src/Components/Testimonials/TestimonialsForm.js
+++ src/Components/Testimonials/TestimonialsForm.js
@@ -1,32 +1,98 @@
 import React, { useState } from 'react';
 import '../FormStyles.css';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+const TestimonialForm = ({ method = 'POST' }) => {
+  const [previewImage, setPreviewImage] = useState();
+  const SUPPORTED_FORMATS = /(jpg|png)/g;
+  const {
+    handleSubmit,
+    handleChange,
+    handleBlur,
+    errors,
+    setFieldTouched,
+    touched,
+    setFieldValue,
+  } = useFormik({
+    initialValues: {
+      name: '',
+      description: '',
+      photo: null,
+    },
 
-const TestimonialForm = () => {
-    const [initialValues, setInitialValues] = useState({
-       name: '',
-       description: '' 
-    });
+    validationSchema: Yup.object({
+      name: Yup.string()
+        .required('El nombre es un campo requerido')
+        .min(4, 'El nombre deberia contenter mas de 4 caracteres'),
+      description: Yup.string().required(
+        'La description es un campo requerido'
+      ),
+      photo: Yup.mixed()
+        .required('La imagen es requerida')
+        .test('fileType', 'El formato no es correcto', (data) => {
+          if (!SUPPORTED_FORMATS.test(data?.type)) return false;
+          setPreviewImage(URL.createObjectURL(data));
+          return true;
+        }),
+    }),
+    onSubmit: handleTestimonialFetch,
+  });
 
-    const handleChange = (e) => {
-        if(e.target.name === 'name'){
-            setInitialValues({...initialValues, name: e.target.value})
-        } if(e.target.name === 'description'){
-            setInitialValues({...initialValues, description: e.target.value})
-        }
-    }
+  function handleTestimonialFetch(data) {
+    console.log(data);
+  }
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(initialValues);
-    }
+  return (
+    <form className="form-container" onSubmit={handleSubmit}>
+      <input
+        className="input-field"
+        type="text"
+        name="name"
+        onChange={handleChange}
+        onBlur={handleBlur}
+        placeholder="Testimonial Title"
+      />
+      <span className="input-error">{touched.name && errors.name}</span>
+      <CKEditor
+        editor={ClassicEditor}
+        onBlur={() => setFieldTouched('description', true)}
+        onChange={(event, editor) => {
+          const data = editor.getData();
+          setFieldValue('description', data);
+        }}
+      />
+      <span className="input-error">
+        {touched.description && errors.description}
+      </span>
 
-    return (
-        <form className="form-container" onSubmit={handleSubmit}>
-            <input className="input-field" type="text" name="name" value={initialValues.name} onChange={handleChange} placeholder="Testimonial Title"></input>
-            <input className="input-field" type="text" name="description" value={initialValues.description} onChange={handleChange} placeholder="Testimonial description"></input>
-            <button className="submit-btn" type="submit">Send</button>
-        </form>
-    );
-}
- 
+      <label className="input-file">
+        Subir imagen
+        <input
+          type="file"
+          accept="image/png, image/jpeg"
+          onChange={(event) => {
+            setFieldTouched('photo', true);
+            setFieldValue('photo', event.target.files[0]);
+          }}
+        />
+      </label>
+      <span className="input-error">{touched.photo && errors.photo}</span>
+      {previewImage && (
+        <img
+          src={previewImage}
+          alt="preview"
+          width={100}
+          height={100}
+          style={{ objectFit: 'cover' }}
+        />
+      )}
+      <button className="submit-btn" type="submit">
+        Send
+      </button>
+    </form>
+  );
+};
+
 export default TestimonialForm;
