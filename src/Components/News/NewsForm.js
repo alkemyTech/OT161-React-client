ade82e9a0de2dd3b7c525e0b85718bf19b821e7d
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2022-03-20 10:06:12 -0300

    refactor: change the original form by a formik form

--- src/Components/News/NewsForm.js
+++ src/Components/News/NewsForm.js
@@ -1,41 +1,76 @@
-import React, { useState } from 'react';
-import '../../Components/FormStyles.css';
+import React, { useState } from "react";
+import "../../Components/FormStyles.css";
+import { Formik } from "formik";
 
 const NewsForm = () => {
-    const [initialValues, setInitialValues] = useState({
-        title: '',
-        content: '',
-        category: ''
-    });
-
-    const handleChange = (e) => {
-        if(e.target.name === 'title'){
-            setInitialValues({...initialValues, title: e.target.value})
-        } if(e.target.name === 'content'){
-            setInitialValues({...initialValues, content: e.target.value})
-        } if(e.target.name === 'category') {
-            setInitialValues({...initialValues, category: e.target.value})
-        }
-    }
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(initialValues);
-    }
-
-    return (
-        <form className="form-container" onSubmit={handleSubmit}>
-            <input className="input-field" type="text" name="title" value={initialValues.title || ''} onChange={handleChange}></input>
-            <input className="input-field" type="text" name="content" value={initialValues.content || ''} onChange={handleChange}></input>
-            <select className="select-field" name="category" value={initialValues.category || ''} onChange={handleChange}>
-                <option value="" disabled>Select category</option>
-                <option value="1">Demo option 1</option>
-                <option value="2">Demo option 2</option>
-                <option value="3">Demo option 3</option>
-            </select>
-            <button className="submit-btn" type="submit">Send</button>
+  return (
+    <Formik
+      initialValues={{ title: "", content: "", category: "" }}
+      //   validate={(values) => {
+      //     const errors = {};
+      //     if (!values.email) {
+      //       errors.email = "Required";
+      //     } else if (
+      //       !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+      //     ) {
+      //       errors.email = "Invalid email address";
+      //     }
+      //     return errors;
+      //   }}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting,
+        /* and other goodies */
+      }) => (
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="title"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.title}
+          />
+          {/* {errors.email && touched.email && errors.email} */}
+          <input
+            type="text"
+            name="content"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.content}
+          />
+          {/* {errors.password && touched.password && errors.password} */}
+          <select
+            className="select-field"
+            name="category"
+            value={values.category || ""}
+            onChange={handleChange}
+          >
+            <option value="" disabled>
+              Select category
+            </option>
+            <option value="1">Demo option 1</option>
+            <option value="2">Demo option 2</option>
+            <option value="3">Demo option 3</option>
+          </select>
+          <button type="submit" disabled={isSubmitting}>
+            Submit
+          </button>
         </form>
-    );
-}
- 
+      )}
+    </Formik>
+  );
+};
+
 export default NewsForm;
