39183b2484202c64e8c6e7cd53ccbf92996b0e06
Jose Manuel Rosales <josemanuelrosales2@gmail.com>
2022-03-21 08:45:47 -0600

    OT161-31: fixed error in post method and proptypes added

--- src/Components/Testimonials/TestimonialsForm.js
+++ src/Components/Testimonials/TestimonialsForm.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
+import PropTypes from 'prop-types';
 /**
  *  Testimonial Form
  * @param {Object} props
@@ -14,121 +15,131 @@ import * as Yup from 'yup';
  * @param {string} props.patchData.description testimonial description
  */
 function TestimonialForm({ patchData }) {
-  const [previewImage, setPreviewImage] = useState(patchData?.image || null);
-  const [statusForm, setStatusForm] = useState(false);
-  const SUPPORTED_FORMATS = /(jpg|png)/;
-  const {
-    handleSubmit,
-    handleChange,
-    handleBlur,
-    values,
-    errors,
-    setFieldTouched,
-    touched,
-    setFieldValue,
-  } = useFormik({
-    initialValues: {
-      name: patchData?.name || '',
-      description: patchData?.description || '',
-      image: patchData?.image || '',
-    },
+	const [previewImage, setPreviewImage] = useState(patchData?.image || null);
+	const [statusForm, setStatusForm] = useState(false);
+	const SUPPORTED_FORMATS = /(jpg|png)/;
+	const {
+		handleSubmit,
+		handleChange,
+		handleBlur,
+		values,
+		errors,
+		setFieldTouched,
+		touched,
+		setFieldValue,
+	} = useFormik({
+		initialValues: {
+			name: patchData?.name || '',
+			description: patchData?.description || '',
+			image: patchData?.image || '',
+		},
 
-    validationSchema: Yup.object({
-      name: Yup.string()
-        .required('El nombre es un campo requerido')
-        .min(4, 'El nombre deberia contenter mas de 4 caracteres'),
-      description: Yup.string().required(
-        'La description es un campo requerido'
-      ),
-      image: Yup.mixed()
-        .required('La imagen es requerida')
-        .test('fileType', 'El formato no es correcto', (image) => {
-          if (!SUPPORTED_FORMATS.test(image)) return false;
-          setPreviewImage(image);
-          return true;
-        }),
-    }),
-    onSubmit: async (testimonial) => {
-      setStatusForm(true);
-      const now = new Date().toISOString();
-      const method = patchData?.id ? 'PATCH' : 'POST';
-      const id = patchData?.id ? patchData.id : '';
-      const url = `https://ongapi.alkemy.org/api/testimonials/${id}`;
-      const data = patchData?.id
-        ? { ...testimonial, updated_at: now }
-        : { ...testimonial, created_at: now };
-      try {
-        const result = await axios({ method, url, data });
-        console.log(result.data);
-      } catch (error) {
-        console.error(error);
-      } finally {
-        setStatusForm(false);
-      }
-    },
-  });
-  function getBase64(file) {
-    return new Promise((resolve, reject) => {
-      const reader = new FileReader();
-      reader.readAsDataURL(file);
-      reader.onload = () => resolve(reader.result);
-      reader.onerror = (error) => reject(error);
-    });
-  }
+		validationSchema: Yup.object({
+			name: Yup.string()
+				.required('El nombre es un campo requerido')
+				.min(4, 'El nombre deberia contenter mas de 4 caracteres'),
+			description: Yup.string().required(
+				'La description es un campo requerido'
+			),
+			image: Yup.mixed()
+				.required('La imagen es requerida')
+				.test('fileType', 'El formato no es correcto', image => {
+					if (!SUPPORTED_FORMATS.test(image)) return false;
+					setPreviewImage(image);
+					return true;
+				}),
+		}),
+		onSubmit: fetchTestimonial,
+	});
+	async function fetchTestimonial(testimonial) {
+		setStatusForm(true);
+		const now = new Date().toISOString();
+		const method = patchData?.id ? 'PATCH' : 'POST';
+		const id = patchData?.id ? `/${patchData.id}` : '';
+		const url = `https://ongapi.alkemy.org/api/testimonials${id}`;
+		const data = patchData?.id
+			? { ...testimonial, updated_at: now }
+			: { ...testimonial, created_at: now };
+		try {
+			const result = await axios({ method, url, data });
+			console.log(result.data);
+		} catch (error) {
+			console.error(error);
+		} finally {
+			setStatusForm(false);
+		}
+	}
+	function getBase64(file) {
+		return new Promise((resolve, reject) => {
+			const reader = new FileReader();
+			reader.readAsDataURL(file);
+			reader.onload = () => resolve(reader.result);
+			reader.onerror = error => reject(error);
+		});
+	}
 
-  return (
-    <form className="form-container" onSubmit={handleSubmit}>
-      <input
-        className="input-field"
-        type="text"
-        name="name"
-        onChange={handleChange}
-        value={values.name}
-        onBlur={handleBlur}
-        placeholder="Testimonial Title"
-      />
-      <span className="input-error">{touched.name && errors.name}</span>
+	return (
+		<form className='form-container' onSubmit={handleSubmit}>
+			<input
+				className='input-field'
+				type='text'
+				name='name'
+				onChange={handleChange}
+				value={values.name}
+				onBlur={handleBlur}
+				placeholder='Testimonial Title'
+			/>
+			<span className='input-error'>{touched.name && errors.name}</span>
 
-      <CKEditor
-        data={values.description}
-        editor={ClassicEditor}
-        onBlur={() => setFieldTouched('description', true)}
-        onChange={(event, editor) => {
-          const data = editor.getData();
-          setFieldValue('description', data);
-        }}
-      />
-      <span className="input-error">
-        {touched.description && errors.description}
-      </span>
+			<CKEditor
+				data={values.description}
+				editor={ClassicEditor}
+				onBlur={() => setFieldTouched('description', true)}
+				onChange={(_, editor) => {
+					const data = editor.getData();
+					setFieldValue('description', data);
+				}}
+			/>
+			<span className='input-error'>
+				{touched.description && errors.description}
+			</span>
 
-      <label className="input-file">
-        Subir imagen
-        <input
-          type="file"
-          accept="image/png, image/jpeg"
-          onChange={async (event) => {
-            setFieldTouched('image', true);
-            const imageBase64 = await getBase64(event.target.files[0]);
-            setFieldValue('image', imageBase64);
-          }}
-        />
-      </label>
-      <span className="input-error">{touched.image && errors.image}</span>
-      {previewImage && (
-        <img
-          src={previewImage}
-          alt="preview"
-          width={100}
-          height={100}
-          style={{ objectFit: 'cover' }}
-        />
-      )}
-      <button className="submit-btn" type="submit" disabled={statusForm}>
-        {patchData?.id ? 'Update' : 'Send'}
-      </button>
-    </form>
-  );
+			<label className='input-file'>
+				Subir imagen
+				<input
+					type='file'
+					accept='image/png, image/jpeg'
+					onChange={async event => {
+						setFieldTouched('image', true);
+						const imageBase64 = await getBase64(event.target.files[0]);
+						setFieldValue('image', imageBase64);
+					}}
+				/>
+			</label>
+			<span className='input-error'>{touched.image && errors.image}</span>
+			{previewImage && (
+				<img
+					src={previewImage}
+					alt='preview'
+					width={100}
+					height={100}
+					style={{ objectFit: 'cover' }}
+				/>
+			)}
+			<button className='submit-btn' type='submit' disabled={statusForm}>
+				{patchData?.id ? 'Update' : 'Send'}
+			</button>
+		</form>
+	);
 }
 
 export default TestimonialForm;
+
+TestimonialForm.propTypes = {
+	patchData: PropTypes.shape({
+		id: PropTypes.number.isRequired,
+		name: PropTypes.string,
+		description: PropTypes.string,
+		image: PropTypes.string,
+	}),
+};
