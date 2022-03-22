8f0ddbe5ba917bc7145b36f62b8da120a8d1222e
Jose Manuel Rosales Gomez <30446712+Josem1801@users.noreply.github.com>
2022-03-21 19:20:53 -0600

    feat: show organization data
    Co-authored-by: Federico Miranda <federico.mirandaa@gmail.com>

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import SchoolCampaign from './Campaigns/School/SchoolCampaign';
 import ToysCampaign from './Campaigns/Toys/ToysCampaign';
 import MembersForm from './Components/Members/MembersForm';
 import ProjectsForm from './Components/Projects/ProjectsForm';
+import OrganizationData from './Components/Organization/OrganizationData';
 import Dashboard from './Components/Dashboard/Dashboard';
 
 function App() {
@@ -25,8 +26,9 @@ function App() {
 					<Route path='/create-category' component={CategoriesForm} />
 					<Route path='/create-news' component={NewsForm} />
 					<Route path='/backoffice/create-slide' component={SlidesForm} />
-					<Route path='/backoffice' component={Dashboard} />
 					<Route path='/create-testimonials' component={TestimonialForm} />
+					<Route path='/backoffice/organization' component={OrganizationData} />
+					<Route path='/backoffice' component={Dashboard} />
 					<Route path='/create-user' component={UserForm} />
 					<Route path='/create-member' component={MembersForm} />
 					<Route path='/create-project' component={ProjectsForm} />
