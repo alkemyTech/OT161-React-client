d3aac48f619ff0803da16e18a8c0803081e8ff43
Jose Manuel Rosales <josemanuelrosales2@gmail.com>
2022-03-24 10:28:26 -0600

    typo: change data jsdoc to postData

--- src/Services/publicApiService.js
+++ src/Services/publicApiService.js
@@ -4,7 +4,7 @@ const BASE_URL = 'https://ongapi.alkemy.org/api';
 /**
  * Function to generate a POST request
  * @param {string} route  Endpoint's route. Example: "/testimonials"
- * @param {Object} data Object with the post data
+ * @param {Object} postData Object with the post data
  */
 async function publicPostRequest(route, postData) {
 	try {
