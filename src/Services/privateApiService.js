1ca89232b87fb4701abd0180ea2344017bfac207
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2022-03-25 14:01:01 -0300

    feat: add private delete request

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -1,15 +1,15 @@
 import axios from 'axios';
+const BASE_URL = 'https://ongapi.alkemy.org/api';
 
-const config = {
-    headers: {
-        Group: 01                //Aqui va el ID del equipo!!
-    }
-}
+const privateDeleteRequest = async ({ url }) => {
+	const headers = { ...tokenFromLocalStorage() };
+	try {
+		const res = await axios.delete(`${BASE_URL}${url}`, headers);
+		console.log(res);
+		return res;
+	} catch (err) {
+		console.log(err);
+	}
+};
 
-const Get = () => {
-    axios.get('https://jsonplaceholder.typicode.com/users', config)
-    .then(res => console.log(res))
-    .catch(err => console.log(err))
-}
-
-export default Get
+export { privateDeleteRequest };
