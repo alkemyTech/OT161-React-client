95635c25ea841e8f51cc3de65d4cb27593b20a5a
Franco De Paulo <83044615+frandepe@users.noreply.github.com>
2022-03-27 03:56:36 -0300

    feat: put method
    Co-authored-by: Federico Miranda <federico.mirandaa@gmail.com>

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -1,6 +1,16 @@
 import axios from 'axios';
 const BASE_URL = 'https://ongapi.alkemy.org/api';
 
+export const privatePutRequest = async ({ url, putData }) => {
+	const headers = { ...tokenFromLocalStorage() };
+	try {
+		const res = await axios.put(`${BASE_URL}${url}`, putData, headers);
+		return res;
+	} catch (err) {
+		console.log(err);
+	}
+};
+
 export const privateDeleteRequest = async ({ url }) => {
 	const headers = { ...tokenFromLocalStorage() };
 	try {
