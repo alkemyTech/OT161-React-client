e047cf8be28a934c06d00c713c29c399595b8653
Franco De Paulo <frandepaulo@yahoo.com.ar>
2022-03-26 23:43:54 -0300

    private put

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -1,15 +1,14 @@
 import axios from 'axios';
+const BASE_URL = 'https://ongapi.alkemy.org/api';
 
-const config = {
-    headers: {
-        Group: 01                //Aqui va el ID del equipo!!
-    }
-}
+const privatePutRequest = async ({ url, putData }) => {
+	const headers = { ...tokenFromLocalStorage() };
+	try {
+		const res = await axios.put(`${BASE_URL}${url}`, putData, headers);
+		return res;
+	} catch (err) {
+		console.log(err);
+	}
+};
 
-const Get = () => {
-    axios.get('https://jsonplaceholder.typicode.com/users', config)
-    .then(res => console.log(res))
-    .catch(err => console.log(err))
-}
-
-export default Get
+export { privatePutRequest };
