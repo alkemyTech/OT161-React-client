37e2610cb83e5cbb6fe0572b3f908b73742094ee
artebleker <artebleker@gmail.com>
2022-03-30 13:40:49 -0300

    fix carrousel

--- src/Components/Slides/Carrousel.js
+++ src/Components/Slides/Carrousel.js
@@ -5,35 +5,50 @@ import parser from "html-react-parser";
 
 const Carrousel = () => {
 
-  const[slides, setSlides] =useState([])
+  const [slides, setSlides] = useState([])
 
   const getData = async () => {
-    const data = await getDataMethod('slides')
-    setSlides(data.data.data)
-    
+    try {
+      const data = await getDataMethod('slides')
+      setSlides(data.data.data)
+    } catch (error) {
+      console.error(error)
+    }
   }
 
-useEffect(()=>{
-  getData()
-},[])
+  useEffect(() => {
+    getData()
+  }, [])
 
 
   const [current, setCurrent] = useState(0);
   const length = slides.length;
 
 
-  const nextSlide = () => {
-    setCurrent(current === length - 1 ? 0 : current + 1);
-  };
-  const prevSlide = () => {
-    setCurrent(current === 0 ? length - 1 : current - 1);
-  };
+  // const nextSlide = () => {
+  //   setCurrent(current === length - 1 ? 0 : current + 1);
+  // };
+  // const prevSlide = () => {
+  //   setCurrent(current === 0 ? length - 1 : current - 1);
+  // };
+  function changeSlide(x) {
+    setCurrent(
+      x < 0 ?
+        current === length - 1 ?
+          0 : current + 1
+        :
+        current === 0 ?
+          length - 1 : current - 1
+    )
+
+  }
+  
   useEffect(() => {
     const interval = setInterval(() => {
-      nextSlide();
-    },5000)
+      changeSlide(1);
+    }, 5000)
     return () => clearInterval(interval);
-  },[]);
+  }, []);
 
 
   if (!Array.isArray(slides) || slides.length <= 0) {
@@ -42,10 +57,10 @@ useEffect(()=>{
 
   return (
     <section className="slider">
-      <div className="left-arrow" onClick={prevSlide}>
+      <div className="left-arrow" onClick={() => changeSlide(1)}>
         &lt;
       </div>
-      <div className="right-arrow" onClick={nextSlide}>
+      <div className="right-arrow" onClick={() => changeSlide(-1)}>
         &gt;
       </div>
       {slides.map((slide, index) => {
@@ -53,9 +68,9 @@ useEffect(()=>{
           <div key={index}>
             {index === current && (
               <div className="mySlides fade">
-                <h2 className="carrousel-title">{slide.title}</h2>
+                <h2 className="carrousel-title">{parser(slide.title)}</h2>
                 <div className="carrousel-image-container">
-                <img src={slide.image} alt="img" className="carrousel-image" />
+                  <img src={slide.image} alt="img" className="carrousel-image" />
                 </div>
                 <p className="carrousel-description">{parser(slide.description)}</p>
               </div>
