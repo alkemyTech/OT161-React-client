9ba967c63be32f7be3232719f1da9d81e079cdd2
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2022-03-30 18:45:45 -0300

    add a rute for backoffice slides

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ import ProjectsForm from './Components/Projects/ProjectsForm';
 import OrganizationData from './Components/Organization/OrganizationData';
 import Dashboard from './Components/Dashboard/Dashboard';
 import NewsDetail from './Components/News/Detail/NewsDetail';
+import SliderList from './Components/Slides/SliderList';
 function App() {
 	return (
 		<>
@@ -29,6 +30,7 @@ function App() {
 					<Route path='/backoffice/create-slide' component={SlidesForm} />
 					<Route path='/create-testimonials' component={TestimonialForm} />
 					<Route path='/backoffice/organization' component={OrganizationData} />
+					<Route path='/backoffice/slides' component={SliderList} />
 					<Route path='/backoffice' component={Dashboard} />
 					<Route path='/create-user' component={UserForm} />
 					<Route path='/create-member' component={MembersForm} />
