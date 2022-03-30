30faee9724e7ba96796babc1081bf0131e387a2b
artebleker <artebleker@gmail.com>
2022-03-30 11:59:16 -0300

    fix cards issues

--- src/Components/Shared/Card/Card.js
+++ src/Components/Shared/Card/Card.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import './card.css'
 import PropTypes from 'prop-types';
-import parser from "html-react-parser";
+
 const Card = ({image = null, title, description}) => {
   return (
-    <section className='cardBody'>
+    <article className='cardBody'>
     {image?
-   <image className='cardImage' src={image} alt={parser(title)}></image>
-   : <div className='cardImage-noImage' ><p>{parser(title)}</p></div>
+   <img className='cardImage' src={image} alt={title}></img>
+   : <div className='cardImage-noImage' ><p>{title}</p></div>
   }
-  <h1 className='cardTitle'>{parser(title)}</h1>
+  <h1 className='cardTitle'>{title}</h1>
 
-   <p className='cardDescription'>{parser(description)}</p>
-    </section>
+   <p className='cardDescription'>{description}</p>
+    </article>
   )
 }
 
