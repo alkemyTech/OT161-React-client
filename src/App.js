ad365f35c031d1de9d70b9b5761709cd20cf5e3f
itsbalexy-rockstar <83509414+itsbalexy-rockstar@users.noreply.github.com>
2022-03-30 20:18:30 -0500

    feat: formulario registro
    * showing registerForm
    
    * validate with formik
    
    * Borrando LoginForm.js para no generar conflictos
    
    Co-authored-by: Federico Miranda <federico.mirandaa@gmail.com>

--- src/App.js
+++ src/App.js
@@ -13,15 +13,16 @@ import SchoolCampaign from './Campaigns/School/SchoolCampaign';
 import ToysCampaign from './Campaigns/Toys/ToysCampaign';
 import MembersForm from './Components/Members/MembersForm';
 import ProjectsForm from './Components/Projects/ProjectsForm';
+import RegisterForm from './Components/Auth/RegisterForm';
 import OrganizationData from './Components/Organization/OrganizationData';
 import Dashboard from './Components/Dashboard/Dashboard';
 import NewsDetail from './Components/News/Detail/NewsDetail';
 function App() {
 	return (
-		<>
 			<BrowserRouter>
 				<Switch>
 					{/* <Route path="/" exact component={} />           Esta ruta debe ser para el Home */}
+          <Route path="/auth/register" component={RegisterForm} />
 					<Route path='/create-activity' component={ActivitiesForm} />
 					<Route path='/create-category' component={CategoriesForm} />
 					<Route path='/backoffice/create-news' component={NewsForm} />
@@ -39,54 +40,6 @@ function App() {
 					<Route path='/novedades/:id' component={NewsDetail} />
 				</Switch>
 			</BrowserRouter>
-			<div className='App'>
-				<header className='App-header'>
-					<img src={logo} className='App-logo' alt='logo' />
-					<Counter />
-					<p>
-						Edit <code>src/App.js</code> and save to reload.
-					</p>
-					<span>
-						<span>Learn </span>
-						<a
-							className='App-link'
-							href='https://reactjs.org/'
-							target='_blank'
-							rel='noopener noreferrer'
-						>
-							React
-						</a>
-						<span>, </span>
-						<a
-							className='App-link'
-							href='https://redux.js.org/'
-							target='_blank'
-							rel='noopener noreferrer'
-						>
-							Redux
-						</a>
-						<span>, </span>
-						<a
-							className='App-link'
-							href='https://redux-toolkit.js.org/'
-							target='_blank'
-							rel='noopener noreferrer'
-						>
-							Redux Toolkit
-						</a>
-						,<span> and </span>
-						<a
-							className='App-link'
-							href='https://react-redux.js.org/'
-							target='_blank'
-							rel='noopener noreferrer'
-						>
-							React Redux
-						</a>
-					</span>
-				</header>
-			</div>
-		</>
 	);
 }
 
