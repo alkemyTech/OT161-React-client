7f40f241327fb804b7fe110db10f80cf0ca7996d
Brayan Alexander Yepes Clavijo <abrayan.claye@gmail.com>
2022-03-30 22:16:07 -0500

    screen-users created

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ import Home from './Components/Home/Home';
 import About from './Components/About/About';
 import Contact from './Components/Contact/Contact';
 import NewsList from './Components/News/NewsList';
+import UsersList from './Components/Users/UsersList';
 
 function App() {
 	return (
@@ -36,6 +37,7 @@ function App() {
 					<Route path='/create-category' component={CategoriesForm} />
 					<Route path='/backoffice/organization/edit' component={HomeForms} />
 					<Route path='/backoffice/news' component={NewsList} />
+					<Route path='/backoffice/users' component={UsersList} />
 					<Route path='/backoffice/create-news' component={NewsForm} />
 					<Route path='/backoffice/create-slide' component={SlidesForm} />
 					<Route path='/create-testimonials' component={TestimonialForm} />
