8f8e481d6623b7706a6ab5edb0ed0976acdd6892
Franco De Paulo <frandepaulo@yahoo.com.ar>
2022-03-31 22:34:10 -0300

    spinner

---
+++ src/shared/Spinner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Rings } from 'react-loader-spinner';
+
+const Spinner = () => {
+	return (
+		<div>
+			<Rings color='#214763' height={80} width={80} />
+		</div>
+	);
+};
+
+export default Spinner;
