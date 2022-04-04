f701247edaa4442501799204c6bdd53605681a93
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2022-04-04 14:59:03 -0300

    feat: add loader y error msg

--- src/Components/Activities/Activities.js
+++ src/Components/Activities/Activities.js
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getDataMethod } from '../../Services/publicApiService';
+import showAlert from '../../shared/showAlert';
 import ShowTitle from '../../shared/ShowTitle';
+import Spinner from '../../shared/Spinner';
 
 const Activities = () => {
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(false);
+	const getData = async () => {
+		try {
+			const data = await getDataMethod('activities');
+			console.log('datos', data.data.data);
+		} catch (error) {
+			console.log(error);
+			setError(true);
+		} finally {
+			setLoading(false);
+		}
+	};
+
+	useEffect(() => {
+		getData();
+	}, []);
+
+	if (error === true) {
+		showError();
+	}
+
 	return (
-		<div>
-			<ShowTitle patchData={{ title: 'Actividades' }}></ShowTitle>
-		</div>
+		<section>
+			<div>
+				<ShowTitle patchData={{ title: 'Actividades' }}></ShowTitle>
+			</div>
+			{loading && <Spinner />}
+		</section>
 	);
 };
 
 export default Activities;
+
+// function that contains showAlert error
+const showError = () => {
+	showAlert({
+		type: 'error',
+		title: 'Ups, hubo un error',
+		message: 'No se pudieron mostrar las actividades, intento mas tarde',
+	});
+};
