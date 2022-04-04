59c05e3d5d36c40ecd6363be2f111dde5a6f0dbb
artebleker <artebleker@gmail.com>
2022-04-04 14:21:36 -0300

    activities reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 import slidesReducer from '../features/Slides/slideSlice';
+import activitiesReducer from '../features/activities/activitiesSlice';
 
 export default configureStore({
 	reducer: {
 		counter: counterReducer,
 		slides: slidesReducer,
+		activities: activitiesReducer,
 	},
 });
