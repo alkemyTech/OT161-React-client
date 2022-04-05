8d974efa40bd878abc4fe82f0552f665aa1a37ab
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2022-04-05 00:13:47 -0300

    feat: add loader, error msg and render the activities

--- src/Components/Activities/Activities.js
+++ src/Components/Activities/Activities.js
@@ -1,16 +1,26 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { getDataMethod } from '../../Services/publicApiService';
 import showAlert from '../../shared/showAlert';
 import ShowTitle from '../../shared/ShowTitle';
 import Spinner from '../../shared/Spinner';
 
 const Activities = () => {
+	const [activitiesData, setActivitiesData] = useState([]);
 	const [loading, setLoading] = useState(true);
 	const [error, setError] = useState(false);
+
+	const errorInfo = {
+		type: 'error',
+		title: 'Ups, hubo un error',
+		message: 'No se pudieron mostrar las actividades, intento mas tarde',
+	};
+
 	const getData = async () => {
 		try {
 			const data = await getDataMethod('activities');
 			console.log('datos', data.data.data);
+			setActivitiesData(data.data.data);
 		} catch (error) {
 			console.log(error);
 			setError(true);
@@ -24,7 +34,7 @@ const Activities = () => {
 	}, []);
 
 	if (error === true) {
-		showError();
+		showAlert(errorInfo);
 	}
 
 	return (
@@ -33,17 +43,17 @@ const Activities = () => {
 				<ShowTitle patchData={{ title: 'Actividades' }}></ShowTitle>
 			</div>
 			{loading && <Spinner />}
+			{activitiesData.map(el => (
+				<div key={el.id}>
+					<img src={el.image} alt={el.name} width='100px' height='100px'></img>
+					<p>{el.name}</p>
+					<Link to={`/actividades/${el.id}`}>
+						<button>Detalles</button>
+					</Link>
+				</div>
+			))}
 		</section>
 	);
 };
 
 export default Activities;
-
-// function that contains showAlert error
-const showError = () => {
-	showAlert({
-		type: 'error',
-		title: 'Ups, hubo un error',
-		message: 'No se pudieron mostrar las actividades, intento mas tarde',
-	});
-};
