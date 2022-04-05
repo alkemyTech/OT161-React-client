cf9449028af596aa21a9959033d6b6166b6efc33
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2022-04-05 14:39:49 -0300

    feat: add error msg to testimonial service

--- src/Services/testimonialService.js
+++ src/Services/testimonialService.js
@@ -1,3 +1,5 @@
+// import showAlert from '../shared/showAlert';
+import showAlert from '../shared/showAlert';
 import getDataMethodPrivate, {
 	privatePatchRequest,
 	privateDeleteRequest,
@@ -8,6 +10,9 @@ export async function getAllTestimonials() {
 		const { data } = await getDataMethodPrivate(
 			process.env.REACT_APP_TESTIMONIAL_END_POINT
 		);
+		// Maneja el error y la alerta. La funcion se encuentra abajo de todo
+		handlError(data, 'No se pudieron mostrar los testimonios');
+
 		return data;
 	} catch (error) {
 		return error;
@@ -20,9 +25,13 @@ export async function createTestimonial(contactData) {
 			process.env.REACT_APP_TESTIMONIAL_END_POINT,
 			contactData
 		);
+
+		handlError(data, 'Lo sentimos, no fue posible cargar los testimonios');
+
 		return data;
-	} catch (error) {
-		return error;
+	} catch (err) {
+		// este catch no funciona
+		console.log(err);
 	}
 }
 export async function getTestimonialById(id) {
@@ -31,6 +40,9 @@ export async function getTestimonialById(id) {
 			process.env.REACT_APP_TESTIMONIAL_END_POINT,
 			id
 		);
+
+		handlError(data, 'Lo sentimos, no fue posible mostrar los resultados');
+
 		return data;
 	} catch (error) {
 		return error;
@@ -42,6 +54,9 @@ export async function updateTestimonial(id, contactData) {
 			`${process.env.REACT_APP_TESTIMONIAL_END_POINT}/${id}`,
 			contactData
 		);
+
+		handlError(data, 'Lo sentimos, no fue posible actualizar los testimonios');
+
 		return data;
 	} catch (error) {
 		return error;
@@ -53,8 +68,26 @@ export async function deleteTestimonial(id) {
 		const { data } = await privateDeleteRequest({
 			url: `${process.env.REACT_APP_TESTIMONIAL_END_POINT}/${id}`,
 		});
+
+		handlError(data, 'Lo sentimos, no fue posible eliminar el testimonio');
+
 		return data;
 	} catch (error) {
 		return error;
 	}
 }
+
+// esta funcion maneja y muestra el error por si la peticion falla
+// Solo se ejecuta si la condicion interna es verdadera
+
+const handlError = (data, message) => {
+	// se utiliza la propiedad "success" que devuelve el back par comprar si la peticion falla
+	if (data.success !== true) {
+		console.log('el Error se muestra Exitosamente');
+		showAlert({
+			type: 'error',
+			title: 'Ups. Hubo un error.',
+			message: message,
+		});
+	}
+};
