a1248f278556c5a6e040b5bdc47e11717e3f8efc
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2022-04-05 00:14:54 -0300

    feat: add loader, error msg and render details of activities

--- src/Components/Activities/Detail/ActivitiesDetail.js
+++ src/Components/Activities/Detail/ActivitiesDetail.js
@@ -1,23 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ShowTitle from './../../../shared/ShowTitle.js';
 import './activitiesDetail.css';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
+import { useParams } from 'react-router-dom';
+import Spinner from '../../../shared/Spinner.js';
+import { getDataMethod } from '../../../Services/publicApiService.js';
+import showAlert from '../../../shared/showAlert.js';
 
+const ActivitiesDetail = () => {
+	const [showDetail, setShowDetail] = useState('');
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(false);
+	const { id } = useParams();
+	console.log('id', id);
 
-const ActivitiesDetail = ({ title, backgroundImage = null, content }) => {
-	
-    return (
+	const errorInfo = {
+		type: 'error',
+		title: 'Ups, hubo un error',
+		message:
+			'No se pudieron mostrar los detalles de las actividades, intento mas tarde',
+	};
+
+	const getActivityDetail = async () => {
+		try {
+			const data = await getDataMethod(`activities/:${id}`);
+			console.log('datos', data.data.data);
+			setShowDetail(data.data.data);
+		} catch (error) {
+			setError(true);
+		} finally {
+			setLoading(false);
+		}
+	};
+
+	useEffect(() => {
+		getActivityDetail();
+	}, []);
+
+	if (error === true) {
+		showAlert(errorInfo);
+	}
+
+	return (
 		<section className='activitiesDetail'>
-			<ShowTitle title={title} background-image={backgroundImage} />
-			<p className='activitiesDetail-content'>{content}</p>
+			{loading && <Spinner />}
+			<ShowTitle
+				patchData={{ title: showDetail.name, background: showDetail.image }}
+			/>
+			<p className='activitiesDetail-content'>{showDetail.description}</p>
 		</section>
 	);
 };
 
 export default ActivitiesDetail;
-
-ActivitiesDetail.propTypes = {
-	title: PropTypes.string,
-	backgroundImage: PropTypes.string,
-	content: PropTypes.string,
-};
