3e42bef6dd32a9f29fb6160008f102283489d555
Brayan Alexander Yepes Clavijo <abrayan.claye@gmail.com>
2022-04-04 23:41:10 -0500

    authReducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 import slidesReducer from '../features/Slides/slideSlice';
+import authReducer from '../features/auth/authSlice';
 
 export default configureStore({
 	reducer: {
 		counter: counterReducer,
 		slides: slidesReducer,
+		auth: authReducer,
 	},
 });
