f8dddc7f73e51c4177d57a374abe4308f3a5f2fa
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2022-04-05 19:24:11 -0300

    refactor: change the sweetalert msg for a normal msg and add tag img for image

--- src/Components/Activities/Detail/ActivitiesDetail.js
+++ src/Components/Activities/Detail/ActivitiesDetail.js
@@ -3,28 +3,23 @@ import ShowTitle from './../../../shared/ShowTitle.js';
 import './activitiesDetail.css';
 import { useParams } from 'react-router-dom';
 import Spinner from '../../../shared/Spinner.js';
-import showAlert from '../../../shared/showAlert.js';
 import getDataMethodPrivate from '../../../Services/privateApiService.js';
+import RenderHTML from '../../../shared/RenderHTML.js';
 
 const ActivitiesDetail = () => {
-	const [showDetail, setShowDetail] = useState('');
+	const [showDetail, setShowDetail] = useState([]);
 	const [loading, setLoading] = useState(true);
 	const [error, setError] = useState(false);
 	const { id } = useParams();
 	console.log('id', id);
 
-	const errorInfo = {
-		type: 'error',
-		title: 'Ups, hubo un error',
-		message:
-			'No se pudieron mostrar los detalles de las actividades, intento mas tarde',
-	};
-
 	const getActivityDetail = async () => {
+		// cambiarlo por el reducer despues
 		try {
 			const data = await getDataMethodPrivate(`activities/${id}`);
 			console.log('datos', data.data.data);
 			setShowDetail(data.data.data);
+			console.log(data.data.data);
 		} catch (error) {
 			setError(true);
 		} finally {
@@ -36,15 +31,24 @@ const ActivitiesDetail = () => {
 		getActivityDetail();
 	}, []);
 
-	if (error === true) {
-		showAlert(errorInfo);
-	}
-
 	return (
-		<section className='activitiesDetail'>
+		//  className='activitiesDetail'
+		<section>
 			{loading && <Spinner />}
+			{error && (
+				<p>
+					Ups. Ocurrio un error. No se pudieron mostrar los detalles de las
+					actividades
+				</p>
+			)}
 			<ShowTitle patchData={{ title: showDetail.name }} />
-			<p className='activitiesDetail-content'>{showDetail.description}</p>
+			<div>
+				<img src={showDetail.image}></img>
+			</div>
+			{/* El renderHTML hace que la clase del <p> no sea tenida en cuenta */}
+			<RenderHTML
+				textHTML={`<p className='activitiesDetail-content'>${showDetail.description}</p>`}
+			/>
 		</section>
 	);
 };
