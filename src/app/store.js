d745c3854411dbcddc37839613aa81b0dc1fd81c
itsbalexy-rockstar <83509414+itsbalexy-rockstar@users.noreply.github.com>
2022-04-05 19:01:13 -0500

    authReducer (#72)
    Co-authored-by: Federico Miranda <federico.mirandaa@gmail.com>

--- src/app/store.js
+++ src/app/store.js
@@ -3,13 +3,14 @@ import counterReducer from '../features/counter/counterSlice';
 import usReducer from '../features/us/usSlice';
 import usersReducer from '../features/users/usersSlice';
 import slidesReducer from '../features/Slides/slideSlice';
+import authReducer from '../features/auth/authSlice';
 
 export default configureStore({
 	reducer: {
 		counter: counterReducer,
 		slides: slidesReducer,
+		auth: authReducer,
     us: usReducer,
     users: usersReducer,
-
 	},
 });
