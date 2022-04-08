aa193cc8869c6df28e561ae69624b038478121ce
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2022-04-08 19:07:30 -0300

    fix: remove switch from react route and let the animatedSwitch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import PublicWebRutes from './routes/PublicWebRutes';
 import BackOfficeRutes from './routes/BackOfficeRutes';
 import { AnimatedSwitch } from 'react-router-transition';
@@ -8,19 +8,17 @@ import { AnimatedSwitch } from 'react-router-transition';
 function App() {
 	return (
 		<BrowserRouter>
-			<Switch>
-				<AnimatedSwitch
-					atEnter={{ opacity: 0 }}
-					atLeave={{ opacity: 0 }}
-					atActive={{ opacity: 1 }}
-					className='switch-wrapper'
-				>
-					<Route>
-						<PublicWebRutes />
-						<BackOfficeRutes />
-					</Route>
-				</AnimatedSwitch>
-			</Switch>
+			<AnimatedSwitch
+				atEnter={{ opacity: 0 }}
+				atLeave={{ opacity: 0 }}
+				atActive={{ opacity: 1 }}
+				className='switch-wrapper'
+			>
+				<Route>
+					<PublicWebRutes />
+					<BackOfficeRutes />
+				</Route>
+			</AnimatedSwitch>
 		</BrowserRouter>
 	);
 }
