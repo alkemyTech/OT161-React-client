4e391f2a37d6b09cd71ae4333f61f48c359afbb9
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2022-04-12 08:26:50 -0300

    fix: fix a few problems in the contact page

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,32 +1,25 @@
-import React, { useEffect, useState } from 'react';
-import { getDataMethod } from '../../Services/publicApiService';
+import React from 'react';
 import ContactForm from './ContactForm';
 import LayoutPublic from '../LayoutPublic/LayoutPublic';
 import ShowTitle from '../../shared/ShowTitle';
 import contactImage from '../../assets/image-contact.png';
 
 const Contact = () => {
-	const [dataContact, setDataContact] = useState([]);
-	const contactONG = async () => {
-		const data = await getDataMethod('organization');
-		setDataContact(data.data.data);
-	};
-
-	useEffect(() => {
-		contactONG();
-	}, []);
-
 	return (
 		<>
-		<LayoutPublic>
-			<div className='contact_container'>
-			<div className='contact_showtitle'>
-				<ShowTitle
-					patchData={{ title: 'Contacte con nosotros', image: contactImage }}
-				/>
-			</div>
-			<ContactForm />
-		</LayoutPublic>
+			<LayoutPublic>
+				<div className='contact_container'>
+					<div className='contact_showtitle'>
+						<ShowTitle
+							patchData={{
+								title: 'Contacte con nosotros',
+								image: contactImage,
+							}}
+						/>
+					</div>
+					<ContactForm />
+				</div>
+			</LayoutPublic>
 		</>
 	);
 };
