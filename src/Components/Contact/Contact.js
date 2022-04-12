da5e4aff8faa98e5620faf068c4b3bf0b1ab3a3f
Franco De Paulo <83044615+frandepe@users.noreply.github.com>
2022-04-11 23:29:17 -0300

    estilos de /contact (#93)
    * estilos de /contact
    
    * estilos contact img
    
    Co-authored-by: Federico Miranda <federico.mirandaa@gmail.com>

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import { getDataMethod } from '../../Services/publicApiService';
+import ContactForm from './ContactForm';
 import LayoutPublic from '../LayoutPublic/LayoutPublic';
-import DataContact from './DataContact';
+import ShowTitle from '../../shared/ShowTitle';
+import contactImage from '../../assets/image-contact.png';
 
 const Contact = () => {
 	const [dataContact, setDataContact] = useState([]);
 	const contactONG = async () => {
 		const data = await getDataMethod('organization');
-		console.log('data:', data.data.data);
 		setDataContact(data.data.data);
 	};
 
@@ -18,18 +19,13 @@ const Contact = () => {
 	return (
 		<>
 		<LayoutPublic>
-			{dataContact && (
-				<DataContact
-					titleContact='Contacto'
-					key={dataContact.id}
-					address={dataContact.address}
-					phone={dataContact.phone}
-					facebook={dataContact.facebook_url}
-					linkedin={dataContact.linkedin_url}
-					instagram={dataContact.instagram_url}
-					twitter={dataContact.twitter_url}
+			<div className='contact_container'>
+			<div className='contact_showtitle'>
+				<ShowTitle
+					patchData={{ title: 'Contacte con nosotros', image: contactImage }}
 				/>
-			)}
+			</div>
+			<ContactForm />
 		</LayoutPublic>
 		</>
 	);
