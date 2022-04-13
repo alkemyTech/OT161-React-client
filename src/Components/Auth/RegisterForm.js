e896beeb8e541092097dc8d435315bbed4ca347c
Franco De Paulo <frandepaulo@yahoo.com.ar>
2022-04-12 21:16:36 -0300

    login y register

--- src/Components/Auth/RegisterForm.js
+++ src/Components/Auth/RegisterForm.js
@@ -1,78 +1,132 @@
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import React from 'react';
 import '../FormStyles.css';
+import { createUser } from '../../Services/UsersHTTPService';
+import showAlert from '../../shared/showAlert';
 
 const RegisterForm = () => {
 	return (
 		<>
 			<Formik
 				initialValues={{
+					name: '',
 					email: '',
 					password: '',
 					confirmPassword: '',
 				}}
 				onSubmit={(values, { resetForm }) => {
 					resetForm();
-					console.log(values)
+					try {
+						createUser(values);
+					} catch (err) {
+						showAlert({
+							type: err,
+							title: 'Ups, hubo un error',
+							message: 'No has podido registrarte, intentelo mas tarde',
+						});
+					}
 				}}
 				validate={values => {
-					const errors = {}
-                    // validate email
+					const errors = {};
+
+					if (!values.name) {
+						errors.name = 'El nombre es obligatorio';
+					}
+					// validate email
 					if (!values.email) {
 						errors.email = 'El email es obligatorio';
 					} else if (
 						!/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(
 							values.email
 						)
-					){
-						errors.email =
-							'Escriba un correo válido';
+					) {
+						errors.email = 'Escriba un correo válido';
 					}
 
-                    // validate password
-                    if(!values.password){
-                        errors.password = 'La contraseña es obligatoria'
-                    } else if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&#.$($)$-$_])[A-Za-z\d$@$!%*?&#.$($)$-$_]{6,15}$/.test(values.password)){
-                        errors.password = 'La contraseña debe terner mínimo 6 caracteres, contener una mayúscula, un número y un carácter especial'
-                    } 
+					// validate password
+					if (!values.password) {
+						errors.password = 'La contraseña es obligatoria';
+					} else if (
+						!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&#.$($)$-$_])[A-Za-z\d$@$!%*?&#.$($)$-$_]{6,15}$/.test(
+							values.password
+						)
+					) {
+						errors.password =
+							'La contraseña debe terner mínimo 6 caracteres, contener una mayúscula, un número y un carácter especial';
+					}
 
-                    if(!values.confirmPassword){
-                        errors.confirmPassword = 'Escriba nuevamente la contraseña'
-                    } else if (!(values.confirmPassword === values.password)){
-                        errors.confirmPassword = 'Ambas contraseñas deben coincidir'
-                    }
+					if (!values.confirmPassword) {
+						errors.confirmPassword = 'Escriba nuevamente la contraseña';
+					} else if (!(values.confirmPassword === values.password)) {
+						errors.confirmPassword = 'Ambas contraseñas deben coincidir';
+					}
 					return errors;
 				}}
 			>
-				{({ errors, touched, handleChange, handleBlur, values }) => (
+				{({ errors, handleChange, handleBlur, values }) => (
 					<Form className='form-container'>
 						<Field
 							className='input-field'
 							type='text'
-							name='email'
-							placeholder='Enter email'
+							name='name'
+							placeholder='Nombre'
+							value={values.name}
+							onChange={handleChange}
+							onBlur={handleBlur}
 						/>
-                        <ErrorMessage name='email' component={() => (
-                            <div className='error-message-form'>{errors.email}</div>
-                        )} />
+						<ErrorMessage
+							name='name'
+							component={() => (
+								<div className='error-message-form'>{errors.name}</div>
+							)}
+						/>
+						<Field
+							className='input-field'
+							type='text'
+							name='email'
+							placeholder='Email'
+							value={values.email}
+							onChange={handleChange}
+							onBlur={handleBlur}
+						/>
+						<ErrorMessage
+							name='email'
+							component={() => (
+								<div className='error-message-form'>{errors.email}</div>
+							)}
+						/>
 						<Field
 							className='input-field'
 							type='password'
 							name='password'
-							placeholder='Enter password'
+							placeholder='Contraseña'
+							value={values.password}
+							onChange={handleChange}
+							onBlur={handleBlur}
 						/>
-                        <ErrorMessage name='password' component={() => (
-                            <div className='error-message-form'>{errors.password}</div>
-                        )} />
+						<ErrorMessage
+							name='password'
+							component={() => (
+								<div className='error-message-form'>{errors.password}</div>
+							)}
+						/>
 						<Field
 							className='input-field'
 							type='password'
 							name='confirmPassword'
-							placeholder='Confirm password'
+							placeholder='Confirme su contraseña'
+							value={values.confirmPassword}
+							onChange={handleChange}
+							onBlur={handleBlur}
 						/>
-                        <ErrorMessage name='confirmPassword' component={() => (
-                            <div className='error-message-form'>{errors.confirmPassword}</div>
-                        )} />
+						<ErrorMessage
+							name='confirmPassword'
+							component={() => (
+								<div className='error-message-form'>
+									{errors.confirmPassword}
+								</div>
+							)}
+						/>
 						<button className='submit-btn' type='submit'>
 							Log In
 						</button>
--- src/Services/UsersHTTPService.js
+++ src/Services/UsersHTTPService.js
@@ -1,67 +1,95 @@
-
-import getDataMethodPrivate, { privatePostRequest, privatePutRequest, privatePatchRequest, privateDeleteRequest } from './privateApiService'
-
+import getDataMethodPrivate, {
+	privatePostRequest,
+	privatePutRequest,
+	privatePatchRequest,
+	privateDeleteRequest,
+} from './privateApiService';
 
 // Get
 const getAllUsers = async (data = null) => {
-    try {
-        const response = await getDataMethodPrivate('/users', data)
-        return response
-    } catch (error) {
-        console.error(error);
-    }
-}
+	try {
+		const response = await getDataMethodPrivate('users', data);
+		return response;
+	} catch (error) {
+		console.error(error);
+	}
+};
 
 // Post
-const createUser = async (data) => {
-    try {
-        const response = await privatePostRequest('/users', data)
-        return response
-    } catch (error) {
-        console.error(error);
-    }
-}
+export const createUser = async data => {
+	try {
+		const response = await privatePostRequest('users', data);
+		return response;
+	} catch (error) {
+		console.error(error);
+	}
+};
 
 // Get {id}
 const getUser = async (id, data = null) => {
-    try {
-        const response = await getDataMethodPrivate(`/users/${id}`, data)
-        return response.data
-    } catch (error) {
-        console.error(error);
-    }
-}
+	try {
+		const response = await getDataMethodPrivate(`users/${id}`, data);
+		return response.data;
+	} catch (error) {
+		console.error(error);
+	}
+};
 
 // Put {id}
 const updateUser = async (id, data) => {
-    try {
-        const response = await privatePutRequest(`/users/${id}`, data)
-        return response
-    } catch (error) {
-        console.error(error);
-    }
-}
+	try {
+		const response = await privatePutRequest(
+			`${process.env.REACT_APP_USERS_ENDPOINT}/${id}`,
+			data
+		);
+		return response;
+	} catch (error) {
+		console.error(error);
+	}
+};
 
 // Patch {id}
 const patchUser = async (id, data) => {
-    try {
-        const response = await privatePatchRequest(`/users/${id}`, data)
-        return response
-    } catch (error) {
-        console.error(error);
-    }
-}
+	try {
+		const response = await privatePatchRequest(
+			`${process.env.REACT_APP_USERS_ENDPOINT}/${id}`,
+			data
+		);
+		return response;
+	} catch (error) {
+		console.error(error);
+	}
+};
 
 // Delete {id}
 const removeUser = async (id, data) => {
-    try {
-        const response = await privateDeleteRequest(`/users/${id}`, data)
-        return response
-    } catch (error) {
-        console.error(error);
-    }
-}
+	try {
+		const response = await privateDeleteRequest(
+			`${process.env.REACT_APP_USERS_ENDPOINT}/${id}`,
+			data
+		);
+		return response;
+	} catch (error) {
+		console.error(error);
+	}
+};
 
+export const loginUser = async data => {
+	try {
+		const response = await privatePostRequest(
+			process.env.REACT_APP_LOGIN_ENDPOINT,
+			data
+		);
+		return response;
+	} catch (error) {
+		console.error(error);
+	}
+};
 
-
-export default { getAllUsers, createUser, getUser, updateUser, patchUser, removeUser }
+export default {
+	getAllUsers,
+	getUser,
+	updateUser,
+	patchUser,
+	removeUser,
+};
