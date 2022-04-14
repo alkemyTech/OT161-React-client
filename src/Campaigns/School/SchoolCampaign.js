a855208513293531f90de8748966268e96c74913
artebleker <artebleker@gmail.com>
2022-04-14 17:09:33 -0300

    campaigns footer responsive

--- src/Campaigns/School/SchoolCampaign.js
+++ src/Campaigns/School/SchoolCampaign.js
@@ -2,15 +2,15 @@ import React from 'react';
 import Header from './Header';
 import Slider from './Slider';
 import Content from './Content';
-import Footer from './Footer';
+import FooterCampaigns from '../footer/FooterCampaigns';
 
 const SchoolCampaign = () => {
   return (
     <>
       <Header />
       <Slider />
-      <Content />
-      <Footer />
+      <Content />    
+      <FooterCampaigns/>
     </>
   );
 }
