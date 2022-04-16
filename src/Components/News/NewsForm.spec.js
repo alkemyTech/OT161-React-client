ded8da8e61ba834ed8e2b473e9b8887c6c6188a2
Jose Manuel Rosales <josemanuelrosales2@gmail.com>
2022-04-16 15:53:51 -0500

    test: validate inputs before clicking submit

--- src/Components/News/NewsForm.spec.js
+++ src/Components/News/NewsForm.spec.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import { screen, render, waitFor, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import NewsForm from './NewsForm';
-describe('News form tests', () => {
+import { BrowserRouter } from 'react-router-dom';
+describe('Formulario de novedades', () => {
 	// Render NewsForm component when start test
-	beforeEach(<NewsForm />);
-
-	it('load and display news form', () => {
-		expect(screen.getByText(/Formulario de novedades/)).toBeInTheDocument();
+	beforeEach(async () => {
+		render(
+			<BrowserRouter>
+				<NewsForm />
+			</BrowserRouter>
+		);
+		await waitFor(() => screen.getByText('Formulario de novedades'));
+	});
+	it('Deberia mostar un error si los campos no estan completos', async () => {
+		fireEvent.click(screen.getByText('Submit'));
+		await waitFor(() => {
+			expect(screen.getByText('El titulo es requerido')).toBeInTheDocument();
+			expect(screen.getByText('El contenido es requerido')).toBeInTheDocument();
+			expect(screen.getByText('La imagen es requerida')).toBeInTheDocument();
+			expect(screen.getByText('La categoria es requerida')).toBeInTheDocument();
+		});
 	});
 });
