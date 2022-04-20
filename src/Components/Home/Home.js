cde289c91c4ae19d172890ee48de73d566fd3739
Franco De Paulo <83044615+frandepe@users.noreply.github.com>
2022-04-19 23:30:50 -0300

    responsive completo usuario (#115)

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,23 +1,24 @@
-import React from 'react'
-import Novedades from './Novedades'
-import Carrousel from '../../Components/Slides/Carrousel'
-import LayoutPublic from '../LayoutPublic/LayoutPublic'
+import React from 'react';
+import Novedades from './Novedades';
+import Carrousel from '../../Components/Slides/Carrousel';
+import LayoutPublic from '../LayoutPublic/LayoutPublic';
+import './Home.css';
 
 const Home = () => {
-  return (
-    <>
-    <LayoutPublic>
-        {/* Este texto viene del endpoint de datos públicos */}
-       <h1>Texto de Bienvenida</h1>
-       <div>
-           <Carrousel />
-       </div>
-       <div>
-           <Novedades />
-       </div>
-       </LayoutPublic>
-    </>
-  )
-}
+	return (
+		<>
+			<LayoutPublic>
+				{/* Este texto viene del endpoint de datos públicos */}
+				<h1 className='home_title'>Somos más</h1>
+				<div>
+					<Carrousel />
+				</div>
+				<div>
+					<Novedades />
+				</div>
+			</LayoutPublic>
+		</>
+	);
+};
 
-export default Home
+export default Home;
