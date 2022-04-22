d70fb196cf922505672ecee27cc6be6f3d13681a
Brayan Alexander Yepes Clavijo <abrayan.claye@gmail.com>
2022-04-22 13:05:17 -0500

    boton volver agg a campañas

--- src/Campaigns/School/Header.js
+++ src/Campaigns/School/Header.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import campaignLogo from '../../assets/campaignImage.png';
 import logoImage from '../../assets/LOGO-SOMOS MAS.png';
+import BotonRegresar from '../../shared/BotonRegresar';
 import './header.css';
 const Header = () => {
 	return (
-		<header className='campaign__school--header'>
-			<img src={campaignLogo} alt='Somos mas - Campaña escolar' />
-			<h1>Campaña escolar</h1>
-			<img src={logoImage} alt='Somos mas' />
-		</header>
+		<>
+			<header className='campaign__school--header'>
+				<img src={campaignLogo} alt='Somos mas - Campaña escolar' />
+				<h1>Campaña escolar</h1>
+				<img src={logoImage} alt='Somos mas' />
+			</header>
+			<BotonRegresar />
+		</>
 	);
 };
 
